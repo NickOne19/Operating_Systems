452fa93e41405af4141ae6d668b737cf382915ab
NickOne19 <49317725+NickOne19@users.noreply.github.com>
2023-09-17 23:34:06 +0300

    Add files via upload

--- Lab3/test/Lab 3.bat
+++ Lab3/test/Lab 3.bat
@@ -2,14 +2,14 @@ Setlocal EnableDelayedExpansion
 @echo off
 set /p direction="Enter the direction: "
 echo Found works:
-for /f "delims=" %%a in ('dir /b /s "C:\Users\OWNER\Desktop\test\atributes\Direction\%direction%\*.lnk"') do (
+for /f "delims=" %%a in ('dir /b /s "C:\Users\OWNER\Desktop\testt\Lab 3 Copy\atributes\Direction\%direction%\*.lnk"') do (
 set "name=%%~na"
 echo !name!
-for /f "delims=" %%b in ('dir /b /s "C:\Users\OWNER\Desktop\test\atributes\!name!.lnk"') do (
+for /f "delims=" %%b in ('dir /b /s "C:\Users\OWNER\Desktop\testt\Lab 3 Copy\atributes\!name!.lnk"') do (
 del "%%b"
 )
-del /Q "C:\Users\OWNER\Desktop\test\science works\!name!\*.*"
-rmdir /Q "C:\Users\OWNER\Desktop\test\science works\!name!"
+del /Q "C:\Users\OWNER\Desktop\testt\Lab 3 Copy\science works\!name!\*.*"
+rmdir /Q "C:\Users\OWNER\Desktop\testt\Lab 3 Copy\science works\!name!"
 )
 
 for /r %%i in (.) do (
